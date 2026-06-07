295dfeae0d28db96cded79cd7efe50069705c35c
Arpan <arpantaneja14@gmail.com>
2026-06-07 15:52:11 +0530

    update

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -94,13 +94,13 @@ export const DATA = {
 
         navbar: true,
       },
-      Resume: {
-        name: "Resume",
-        url: "https://drive.google.com/drive/folders/1qH3eRSYjimIg_ryPR-7c5HSkvGkyPkih?usp=sharing",
-        icon: Icons.file,
+      // Resume: {
+      //   name: "Resume",
+      //   url: "https://drive.google.com/drive/folders/1qH3eRSYjimIg_ryPR-7c5HSkvGkyPkih?usp=sharing",
+      //   icon: Icons.file,
 
-        navbar: true,
-      },
+      //   navbar: true,
+      // },
       email: {
         name: "Send Email",
         url: "#",
